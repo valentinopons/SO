030e4330aced0ed8ed378116f9a1f26f41ae65ab
vpons <valentinopons@gmail.com>
2025-04-05 11:28:43 -0300

    ejercicio 5b programado

--- guia1/ejercico5.c
+++ guia1/ejercico5.c
@@ -11,35 +11,41 @@
 
 
 */
+
+
+
  void soyYo(char* nombre){
-    printf("soy %s y mi pid es: %d\n", nombre ,getpid());
+    printf("soy %s y ya termine", nombre);
     exit(0);
  }
+
 void imprimirFamilia(){
-    printf("soy Abrham y mi pid es: %d\n", getpid() );
     __pid_t pid = fork();
-    if (pid == 0)
-    {
-        printf("soy Homero y mi pid es: %d\n", getpid());
-        
+    if (pid == 0){    
         __pid_t bart = fork();
-        if (bart == 0)
-        {
+        if (bart == 0){
             soyYo("bart");
         }
         __pid_t lisa = fork();
-        if (lisa == 0)
-        {
+        if (lisa == 0){
             soyYo("lisa");
         }
         __pid_t magie = fork();
-        if ( magie == 0)
-        {
+        if ( magie == 0){
             soyYo("magie");
         }
+        __pid_t pidh;
+        while (pidh = wait(NULL) > 0){
+            printf("termina hijo \n");
+        }
+        printf("termnina homero \n");
         
-        
+    }else{
+        wait(NULL);
+        printf("Abrham termino");
     }
+    
+    
 
     
 }
